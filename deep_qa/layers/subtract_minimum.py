d74031ec86631c27179d68c28d5ec5e3e7d6f6a3
Matt Gardner <mattg@allenai.org>
2017-04-28 22:47:09 +0000

    Fixes recommended in code review

--- deep_qa/layers/subtract_minimum.py
+++ deep_qa/layers/subtract_minimum.py
@@ -1,45 +1,48 @@
 from keras import backend as K
+from overrides import overrides
 
 from deep_qa.layers.masked_layer import MaskedLayer
 
 class SubtractMinimum(MaskedLayer):
     '''
-    This layer is used to normalize across a tensor axis.  Normalization is done by finding the minimum value
-    across the specified axis, and then subtracting that value from all values (again, across the spcified axis).
+    This layer is used to normalize across a tensor axis.  Normalization is done by finding the
+    minimum value across the specified axis, and then subtracting that value from all values
+    (again, across the spcified axis).  Note that this also works just fine if you want to find the
+    minimum across more than one axis.
+
+    Inputs:
+        - A tensor with arbitrary dimension, and a mask of the same shape (currently doesn't
+          support masks with other shapes).
+
+    Output:
+        - The same tensor, with the minimum across one (or more) of the dimensions subtracted.
+
+    Parameters
+    ----------
+    axis: int
+        The axis (or axes) across which to find the minimum.  Can be a single int, a list of ints,
+        or None.  We just call `K.min` with this parameter, so anything that's valid there works
+        here too.
     '''
-    def __init__(self, axis, **kwargs):
+    def __init__(self, axis: int, **kwargs):
         self.axis = axis
         super(SubtractMinimum, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        # Add the trainable weight variable for the noise parameter.
-        super(SubtractMinimum, self).build(input_shape)
-
-    def get_output_shape_for(self, input_shape): # pylint: disable=no-self-use
+    @overrides
+    def compute_output_shape(self, input_shape): # pylint: disable=no-self-use
         return input_shape
 
+    @overrides
     def compute_mask(self, inputs, mask=None):
         return mask
 
+    @overrides
     def call(self, inputs, mask=None):
-        axis_dimension = K.int_shape(inputs)[self.axis]
-        tile_dimensions = [1] * K.ndim(inputs)
-        tile_dimensions[self.axis] = axis_dimension
-
         if mask is not None:
-            # # Handle the case where the mask is one-dimension smaller than the inputs to mask all values for that
-            # # masked vector.
-            # if K.ndim(mask) == K.ndim(inputs) - 1:
-            #     mask = K.expand_dims(mask)
-            #     mask_tile_dimensions = [1] * K.ndim(inputs)
-            #     mask_tile_dimensions[-1] = K.int_shape(inputs)[-1]
-            #     mask = K.tile(mask, mask_tile_dimensions)
-            # # Make all masked values very large
-            mask_flipped_and_scaled = K.cast(K.equal(mask, K.zeros_like(mask)), "float32") * 1000000.0
-            dim_1_minimums = K.min(inputs + mask_flipped_and_scaled, axis=self.axis, keepdims=True)
+            # Make all masked values very large.
+            mask_flipped_and_scaled = K.cast(K.equal(mask, 0), "float32") * K.max(inputs)
+            minimums = K.min(inputs + mask_flipped_and_scaled, axis=self.axis, keepdims=True)
         else:
-            dim_1_minimums = K.min(inputs, axis=self.axis, keepdims=True)
-
-        minimums = K.tile(dim_1_minimums, tile_dimensions)
+            minimums = K.min(inputs, axis=self.axis, keepdims=True)
         normalized = inputs - minimums
         return normalized
